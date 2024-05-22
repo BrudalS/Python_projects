4472d5b6b388de44435c23fad6e1a12bec5f12ef
BrudalS <123343570+BrudalS@users.noreply.github.com>
2024-05-22 16:17:34 +0530

    Added new game

--- Begginer/random_number_guess.py
+++ Begginer/random_number_guess.py
@@ -2,6 +2,8 @@ import random
 
 random_limit = input("Enter the limit for the random number ")
 
+guess = 0
+
 if random_limit.isdigit():
 	random_limit = int(random_limit)
 
@@ -11,17 +13,24 @@ else:
 
 while True:
 	user_number = input("Enter ur guess ")
-	random_number = random.randint(0,random_limit)
+	random_number = random.randint(0,  random_limit)
 
 	if user_number.isdigit():
 		user_number = int(user_number)
 		print(f"Random generated number is {random_number}")
 
 		if random_number == user_number:
-			print("U won ")
-			quit()
+			print("U got it ")
+			guess += 1
+			break
+
+		else:
+			guess += 1
+			continue
 
 	else:
-		print("Invalid input")
-		break
+		print("Enter a number next time")
 
+
+
+print(f"The number of guesses u took to crack it is {guess}")
---
+++ Begginer/rock_paper_scissors.py
@@ -0,0 +1,4 @@
+import random
+
+options = ["rock", "paper", "sessior"]
+print(dict(x,k for x,k in options))
